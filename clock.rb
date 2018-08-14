8bd9e1ea7f7bf9951e92e3d61ae0c0755967148a
Laura <larmartinez2@gmail.com>
2018-08-14 10:46:25 -0700

    method to check valid values for hours, min, and secc

--- clock.rb
+++ clock.rb
@@ -1,1 +1,21 @@
 # clock.rb
+# clock method
+def clock(hours, min, sec)
+  if hours > 23 || hours < 0
+    raise ArgumentError, "Hours must be between 0-23"
+  end
+
+  if hours < 10
+    hours = "0#{hours}"
+  end
+
+  if min < 10
+    min = "0#{min}"
+  end
+
+  if sec < 10
+    sec = "0#{sec}"
+  end
+
+  return "#{hours}:#{min}:#{sec}"
+end
